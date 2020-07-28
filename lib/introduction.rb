33132c6da2f382eae73925ab722e36f7f452ba53
Kelin Field <kfield111@hotmail.com>
2020-07-28 18:35:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,5 @@
-name = "Kelin"
-language = "Javascript"
-
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+introduction_with_language(Kelin, Javascript)
